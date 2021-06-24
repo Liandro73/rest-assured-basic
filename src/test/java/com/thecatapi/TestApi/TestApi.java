f197f84207a96ba833b4223a1dae8bf0a27391e9
Dernival Liandro <liandro.sys@gmail.com>
2021-06-24 20:14:42 -0300

    Adicionando um favorito, e deletando favorito

--- src/test/java/com/thecatapi/TestApi/TestApi.java
+++ src/test/java/com/thecatapi/TestApi/TestApi.java
@@ -9,6 +9,7 @@ import static org.hamcrest.Matchers.containsString;
 public class TestApi {
 
     private String vote_id;
+    private String favourite_id;
 
     @Test
     public void cadastro() {
@@ -17,7 +18,11 @@ public class TestApi {
         final String corpoCadastro =
                 "{ \"email\": \"dernival.liandro@foton.la\", \"appDescription\": \"Test Api\", \"details\": { \"user_type\": \"personal\"}}";
 
-        Response response = given().contentType("application/json").body(corpoCadastro).when().post(url);
+        Response response = given()
+                .contentType("application/json")
+                .body(corpoCadastro)
+                .when().post(url);
+
         response.then().body("message", containsString("SUCCESS")).statusCode(200);
 
         System.out.println("Retorno do cadastro " + response.body().asString());
@@ -27,10 +32,13 @@ public class TestApi {
     public void votacao() {
 
         final String url = "https://api.thecatapi.com/v1/votes/";
-        final String corpoVoto =
-                "{\"image_id\": \"61h\", \"value\": \"true\", \"sub_id\": \"demo-c26f9f\"}";
+        final String corpoVoto = "{\"image_id\": \"61h\", \"value\": \"true\", \"sub_id\": \"demo-c26f9f\"}";
 
-        Response response = given().contentType("application/json").body(corpoVoto).when().post(url);
+        Response response = given()
+                .contentType("application/json")
+                .body(corpoVoto)
+                .when().post(url);
+
         response.then().body("message", containsString("SUCCESS")).statusCode(200);
 
         System.out.println("Retorno da votacao " + response.body().asString());
@@ -40,12 +48,12 @@ public class TestApi {
 
     }
 
-
     public void deletaVoto() {
 
         final String url = "https://api.thecatapi.com/v1/votes/{vote_id}";
 
-        Response response = given().contentType("application/json")
+        Response response = given()
+                .contentType("application/json")
                 .header("x-api-key", "f5651d2c-f65e-4f2c-8a20-d1f720191869")
                 .pathParam("vote_id", vote_id)
                 .when().delete(url);
@@ -54,13 +62,54 @@ public class TestApi {
 
         response.then().body("message", containsString("SUCCESS")).statusCode(200);
 
-
     }
 
     @Test
     public void deletaVotacao() {
         votacao();
         deletaVoto();
+    }
+
+    public void favoritar() {
+
+        final String url = "https://api.thecatapi.com/v1/favourites";
+        final String corpoFavorito = "{\"image_id\": \"bt0\", \"sub_id\": \"demo-c26f9f\"}";
+
+        Response response = given()
+                .contentType("application/json")
+                .header("x-api-key", "f5651d2c-f65e-4f2c-8a20-d1f720191869")
+                .body(corpoFavorito)
+                .when().post(url);
+
+        response.then().body("message", containsString("SUCCESS")).statusCode(200);
+
+        System.out.println("Retorno da votacao " + response.body().asString());
+        String id = response.jsonPath().getString("id");
+        favourite_id = id;
+        System.out.println("ID da votacao " + id);
+
+    }
+
+    public void deletaFavorito() {
+
+        final String url = "https://api.thecatapi.com/v1/favourites/{favourite_id}";
+
+        Response response = given()
+                .contentType("application/json")
+                .header("x-api-key", "f5651d2c-f65e-4f2c-8a20-d1f720191869")
+                .pathParam("favourite_id", favourite_id)
+                .when().delete(url);
+
+        System.out.println("Retorno da delecao do voto " + response.body().asString());
+
+        response.then().body("message", containsString("SUCCESS")).statusCode(200);
+
+    }
+
+    @Test
+    public void executarFavoritarDeletar() {
+        favoritar();
+        deletaFavorito();
     }
 
 }
