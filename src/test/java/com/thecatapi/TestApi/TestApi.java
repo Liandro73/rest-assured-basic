91bf1d66a121bee7c3e19b289eaec50c309bc3bb
Dernival Liandro <liandro.sys@gmail.com>
2021-06-24 11:35:51 -0300

    Testando API de votacao

--- src/test/java/com/thecatapi/TestApi/TestApi.java
+++ src/test/java/com/thecatapi/TestApi/TestApi.java
@@ -13,12 +13,28 @@ public class TestApi {
 
         final String url = "https://api.thecatapi.com/v1/user/passwordlesssignup";
         final String corpoCadastro =
-                "{ \"email\": \"testcatapi@gmail.com\", \"appDescription\": \"Test Api\", \"details\": { \"user_type\": \"personal\"}}";
+                "{ \"email\": \"testcatapi1@gmail.com\", \"appDescription\": \"Test Api\", \"details\": { \"user_type\": \"personal\"}}";
 
         Response response = given().contentType("application/json").body(corpoCadastro).when().post(url);
         response.then().body("message", containsString("SUCCESS")).statusCode(200);
 
         System.out.println("Retorno do cadastro" + response.body().asString());
+
+    }
+
+    @Test
+    public void votacao() {
+
+        final String url = "https://api.thecatapi.com/v1/votes/";
+        final String corpoVoto =
+                "{\"image_id\": \"61h\", \"value\": \"true\", \"sub_id\": \"demo-c26f9f\"}";
+
+        Response response = given().contentType("application/json").body(corpoVoto).when().post(url);
+        response.then().body("message", containsString("SUCCESS")).statusCode(200);
+
+        System.out.println("Retorno da votacao" + response.body().asString());
+        String id = response.jsonPath().getString("id");
+        System.out.println("ID da votacao " + id);
 
     }
 
